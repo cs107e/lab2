3850c146195c7370ca1c0e929ba9b44c3642bccf
Liana Keesing <63247581+liana-keesing@users.noreply.github.com>
2021-01-26 19:49:33 +0000

    Merge pull request #321 from cs107e/lab2-update commit 2d3fa73c29cf23aceb1f5661012a9204e4253f7b Author: Liana Keesing <63247581+liana-keesing@users.noreply.github.com> Date:   Tue Jan 26 11:45:00 2021 -0800
        Merge pull request #321 from cs107e/lab2-update
    
        lab2 update

--- code/testing/testing.c
+++ code/testing/testing.c
@@ -2,18 +2,40 @@
 // ---------------
 // Unit testing example
 
-#include "assert.h"
+// You call assert on an expression that you expect to be true. If expr
+// actually evaluates to false, then assert calls abort, which stops
+// your program and flashes the red light of doom.
+#define assert(expr) if(!(expr)) abort()
+
+// infinite loop that flashes the red power LED (GPIO 35)
+void abort(void) {
+    int delay = 0x100000;
+    int bit35 =  1 << 3;
+
+    // Configure GPIO 35 function to be output.
+    // This wipes functions for other gpios in this register (30-39)
+    // but that's okay, because this is a dead-end routine.
+    *((volatile unsigned int *)0x2020000c) = 1 << 15; // GPIO_FSEL3
+    while (1) {
+        *((volatile unsigned int *)0x20200020) = bit35;  // GPIO_SEL1
+        // Delay loop uses volatile counters to prevent being optimized out
+        for (volatile int i = 0; i < delay; i++) ;
+        *((volatile unsigned int *)0x2020002c) = bit35; // GPIO_CLR1
+        for (volatile int i = 0; i < delay; i++) ;
+    }
+}
 
 // The count_bits function is intended to return the count of
-// on bits in a numeric value. But oops-- the code as 
+// on bits in a numeric value. But oops-- the code as
 // written below is buggy!  A program this simple we could try to
 // validate "by inspection" but instead we will demonstrate
 // how to find the problem using rudimentary unit tests and
-// the assert() function.
+// the simple assert macro defined above.
+//
 // You assert a condition that must be true and if the expression
 // evaluates to false, assert treats as an error.
-// We use the red and greeen LEDs on the Pi board as status indicators. 
-// When an assert fails, it stops executing the program
+// We use the red and greeen LEDs on the Pi board as status indicators.
+// When an assert fails, it calls abort(), which stops here
 // and flashes the red LED. If the program successfully finishes
 // executing (no assert failed), the green LED turns on.
 // Thus the flashing red LED of doom tells you that your
@@ -26,7 +48,7 @@ int count_bits(unsigned int val)
     int count = 0;
 
     while (val != 0) {
-        val = val >> 1;   // BUGGY: should shift AFTER testing low bit 
+        val = val >> 1;   // BUGGY: should test/count lsb first, then shift
         if (val & 1)      // which inputs are affected by this bug?
             count++;
     }
@@ -34,7 +56,7 @@ int count_bits(unsigned int val)
 }
 
 
-void main(void) 
+void main(void)
 {
     assert(count_bits(0) == 0);
     assert(count_bits(8) == 1);
